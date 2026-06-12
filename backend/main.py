e4e6b9f160a0db8ab1457c241d91ff53d03865ca
RaduTek <me@radutek.uk>
2026-06-12 09:24:41 +0300

    Enable CORS for localhost testing

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from .database import engine
@@ -15,5 +16,22 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Transio Server", lifespan=lifespan)
+
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:8000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
